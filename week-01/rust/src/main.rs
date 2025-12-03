c303ab0a339aedb96916c3ec0d055d6340b52a6a
Salman <salmankhan.yt135@gmail.com>
2025-12-03 16:46:53 +0500

    writing program in each lang

--- week-01/rust/src/main.rs
+++ week-01/rust/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let mut sum=0;
+    let numbers=[5,3,2,1,6,3];
+    for i in numbers{
+        sum+=i
+    }
+    println!(
+        "{}",sum
+    )
 }
+
